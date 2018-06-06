7a60cce9a6593bc4fe7814f1a4fa30fda4199acd
ekeough6 <erickeough6@gmail.com>
2018-06-06 13:12:05 -0400

    Hovering functionality added, working on restoring reloading abilities

--- script.js
+++ script.js
@@ -12,7 +12,6 @@
     var serial;
     var previousRanges = [];
     rangy.init();
-    //applier = rangy.createClassApplier("highlightText");
     applierCount = rangy.createClassApplier("hl" + count);
     var userData;
     var enableInterval;
@@ -20,13 +19,16 @@
 
     function loadData() {
       var deferred = new $.Deferred();
+      $("#dialog").css({
+        "visibility": "hidden"
+      });
       $.get('load.php', function(result) {
         commentData = JSON.parse(result);
 
         currentUser = commentData.netid;
         console.log(commentData);
         commentData.allUsers.forEach(function(element) {
-          //console.log(element);
+          console.log(element);
           if (element.name === currentUser) {
             element.comments.forEach(function(comment, index) {
               console.log(comment);
@@ -49,16 +51,7 @@
 
 
     $("#text").on("mouseup", function() {
-
-      //  $('pre')[0].prepend("<p>Test</p>")
-      // $('pre')[0].append("<p>Test</p>")
-      //highlightSelection()
-      //highlighter.highlightSelection("highlightText", exclusive=false);
-      //restoreHighlights();
       highlightCurrentSelection();
-
-
-      //postContent();
       $('#text').css("pointer-events", "none");
     });
 
@@ -77,13 +70,51 @@
       return sel.rangeCount ? sel.getRangeAt(0) : null;
     }
 
+    function highlightCurrentSelection() {
+      if (getFirstRange().endOffset != getFirstRange().startOffset) {
+        serial = rangy.serializeRange(getFirstRange());
+        applierCount.applyToSelection();
+        previousRanges.push(getFirstRange());
+        //linkComments(count);
+        //$("#commentForm")[0].reset();
+        count++;
+        applierCount = rangy.createClassApplier("hl" + count);
+        highlightPrompt();
+      }
+    }
+
+    function highlightRange(range) {
+      applierCount.applyToRange(range);
+      //linkComments(count);
+      count++;
+      applierCount = rangy.createClassApplier("hl" + count);
+
+    }
+
+    function unhighlightRange(range) {
+      count--;
+      applierCount = rangy.createClassApplier("hl" + count);
+      applierCount.applyToRange(range);
+    }
+
+    function unhighlightCount(count) {
+      applierCount = rangy.createClassApplier("hl" + count);
+      var el = document.getElementById("text");
+      var range = rangy.createRange();
+      range.selectNode(el);
+      applierCount.undoToRange(range);
+    }
+
+    function unhighlightPreviousRange() {
+      unhighlightCount(--count);
+    }
+
     function getHighlightedRanges() {
       var ranges = [];
       for (i = 0; i < count; i++) {
         var elements = document.getElementsByClassName("hl" + i);
         Array.prototype.forEach.call(elements, function(el) {
           var range = rangy.createRange(el);
-          console.log(el);
           if (ranges[i]) {
             ranges[i].union(range);
           } else {
@@ -94,36 +125,12 @@
       return ranges;
     }
 
-    function highlightCurrentSelection() {
-      //previousRanges = previousRanges.length ? unhighlightPreviousRange(previousRanges) : [];
-      serial = rangy.serializeRange(getFirstRange());
-
-      //applier.applyToSelection();
-      applierCount.applyToSelection();
-      previousRanges.push(getFirstRange());
-      applierCount = rangy.createClassApplier("hl" + count);
-
-      highlightPrompt();
-    }
-
-    function highlightRange(range) {
-      //applier.applyToRange(range);
-      applierCount.applyToRange(range);
-      highlightPrompt();
-      applierCount = rangy.createClassApplier("hl" + count);
-    }
-
-    function unhighlightRange(range) {
-      //applier.undoToRange(range);
-      count--;
-      applierCount = rangy.createClassApplier("hl" + count);
-      applierCount.undoToRange(range);
-      highlightPrompt();
-    }
-
-    function unhighlightPreviousRange(ranges) {
-      unhighlightRange(ranges.pop());
-      return ranges;
+    function restoreHighlights() {
+      comments.forEach(function(comment) {
+        $("#commentForm")[0].reset();
+        var range = rangy.deserializeRange(comment.serial);
+        highlightRange(rangy.deserializeRange(comment.serial));
+      });
     }
 
     function highlightPrompt() {
@@ -135,60 +142,74 @@
       $("#dialog").css({
         "visibility": "visible"
       });
-      $("#dialog").dialog();
       $("#dialog").dialog({
-        modal: true
+        dialogClass: "no-close",
+        modal: true,
+        buttons: [{
+            text: "Remove",
+            click: function() {
+              $(this).css({
+                "visibility": "hidden"
+              });
+              $(this).dialog("destroy");
+              unhighlightPreviousRange();
+            }
+          },
+          {
+            text: "Save",
+            click: function() {
+              $(this).css({
+                "visibility": "hidden"
+              });
+              $(this).dialog("destroy");
+              postContent();
+            }
+          }
+        ]
       });
-      count++;
     }
 
-    function restoreHighlights() {
-      comments.forEach(function(comment) {
-        var range = rangy.deserializeRange(comment.serial);
-        highlightRange(rangy.deserializeRange(comment.serial));
-      });
-      console.log(getHighlightedRanges());
-
-    }
-
-    //      $('span').on("mouseover", function () {
-    //          if (this.hasClass("highlightText")) {
-    //              if ($("#dialog").css('visibility') == "hidden") {
-    //                  $("#dialog").css({
-    //                      "visibility": "visible"
-    //                  });
-    //                  $("#dialog").dialog();
-    //                  $("#commentForm input[name=start]").val(tagInfo.start);
-    //                  $("#commentForm input[name=end]").val(tagInfo.end);
-    //              }
-    //          }
-    //      });
-
-    function getRange() {
-      var rangePromise = jQuery.Deferred();
-      var range = window.getSelection().getRangeAt(0);
-      rangePromise.resolve(range)
-      return rangePromise.promise();
-    }
-
-    function highlightSelection() {
-      //window.getSelection().getRangeAt(0).startContainer.prepend("<text>fdsfa</text>")
-      //console.log(window.getSelection().getRangeAt(0).endContainer)
-      //highlightRange(window.getSelection().getRangeAt(0))
-      //            ranges.push(window.getSelection().getRangeAt(0));
-      $.when(getRange()).then(
-        function(range) {
-          console.log(range)
-          maketagAndDialog({
-            start: range.startOffset,
-            end: range.endOffset
-          })
+    function linkComments(num) {
+      $(".hl" + num).on("mouseover", function(e) {
+        $("#dialog").css({
+          "visibility": "visible"
         });
-      //            var safeRanges = getSafeRanges(userSelection);
-      //            for (var i = 0; i < safeRanges.length; i++) {
-      //                highlightRange(safeRanges[i]);
-      //            }
+        $("#dialog").dialog({
+          buttons: [{
+            text: "Close",
+            click: function() {
+              $(this).dialog("close");
+            }
+          }]
+        });
+        infoDialog(num);
+      });
     }
+
+    function infoDialog(commentNum) {
+
+      var comment = getComment(commentNum);
+      for (i in comment) {
+        $("[name=" + i + "]").val(comment[i]);
+      }
+
+      $("#dialog").css({
+        "visibility": "visible"
+      });
+      $("#dialog").dialog();
+      // $("#commentForm input[name=start]").val(tagInfo.start);
+      // $("#commentForm input[name=end]").val(tagInfo.end);
+    }
+
+    function getComment(commentNum) {
+      var com = comments.find(function(item) {
+        return item.start == commentNum;
+      });
+      return com;
+    }
+
+
+
 
 
     function maketagAndDialog(tagInfo) {
@@ -237,12 +258,6 @@
         });
         $("#dialog").dialog();
         infoDialog(tagInfo);
-
-        //console.log("tagInfo.start: ", tagInfo.start);
-        //console.log("tagInfo.end: ", tagInfo.end);
-
-        // $("#commentForm input[name=name]").val(tagInfo.start);
-        // $("#commentForm input[name=field1]").val(tagInfo.end);
       });
 
 
@@ -265,60 +280,44 @@
     }
 
 
-    function infoDialog(tagInfo) {
-
-      var comment = returnComment(tagInfo.commentID) || {
-        "commentID": "temp_" + Date.now(),
-        "start": tagInfo.start,
-        "end": tagInfo.end
-      };
-
-      console.log(comment)
-      for (i in comment) {
-
-        $("[name=" + i + "]").val(comment[i])
-
-
-      }
-
-      $("#dialog").css({
-        "visibility": "visible"
-      });
-      $("#dialog").dialog();
-      //          $("#commentForm input[name=start]").val(tagInfo.start);
-      //
-      //          $("#commentForm input[name=end]").val(tagInfo.end);
-    }
+    // function infoDialog(tagInfo) {
+    //
+    //   var comment = returnComment(tagInfo.commentID) || {
+    //     "commentID": "temp_" + Date.now(),
+    //     "start": tagInfo.start,
+    //     "end": tagInfo.end
+    //   };
+    //
+    //   console.log(comment)
+    //   for (i in comment) {
+    //
+    //     $("[name=" + i + "]").val(comment[i])
+    //
+    //
+    //   }
+    //
+    //   $("#dialog").css({
+    //     "visibility": "visible"
+    //   });
+    //   $("#dialog").dialog();
+    //   //          $("#commentForm input[name=start]").val(tagInfo.start);
+    //   //
+    //   //          $("#commentForm input[name=end]").val(tagInfo.end);
+    // }
 
     function postContent() {
       var data = $("#commentForm").serializeFormJSON();
       data.serial = serial;
+      data.start = count - 1;
       console.log(data);
+      comments.push(data);
       $.post("save.php", {
         data: JSON.stringify(data)
       });
       previousRanges = [];
+      //$("#commentForm")[0].reset();
 
     }
-    /*if (enableInterval) {
-          setInterval(function () {
-
-              var preLength = $('pre').length
-              $("pre").each(function (index) {
-
-                  var zIndex = (parseInt($(this).css('zIndex')) + 1) % preLength;
-                  $(this).css('zIndex', zIndex)
-              })
-          }, 100);
-      }
-*/
-
-    /*function highlightRange(range) {
-        newNode = document.createElement("param");
-        range.endContainer.appendChild(newNode);
-        //range.selectNode(document.getElementsByTagName("div").item(0));
-        range.insertNode(newNode);
-    }*/
 
     (function($) {
       $.fn.serializeFormJSON = function() {
